672d435a7444d29444286ff93a51f09e40e5c5e6
Riaan <riaan.swanepoel@digitata.com>
2019-05-04 13:36:21 +0200

    Added basic layout

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-toolbar app dark>
+      <v-avatar color="transparent">
+          <img :src="require('./assets/ff_embliem_small.png')" alt="avatar">
+        </v-avatar>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>BOARDROOM</span>
+        <span class="font-weight-light">SCHEDULER</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content app>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fill-height fluid fullscreen ma-0 pa-0>
+    <v-layout>
+      <v-flex>
+        <vue-cal />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
+
+export default {
+  components: { VueCal }
+};
+</script>
