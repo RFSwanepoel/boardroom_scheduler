f206aeb93407d989171fe408ed806bc510452a60
Riaan <swanepoel.rf@gmail.com>
2019-05-13 04:55:35 +0200

    ...

--- src/router.js
+++ src/router.js
@@ -16,7 +16,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/home.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Home.vue')
     },
     {
       path: '/report',
